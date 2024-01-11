453b87811c1b87956ed79fa091a832e114c1ab16
Aniko Sallai <aniko.sallai@gmail.com>
2024-01-11 15:27:16 +0100

    136. Functions & Arrow Functions

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ console.log("Hello world")
 console.info("Hello world")
 console.warn("warning")
 console.error("error")
+
+function add(num1, num2) {
+    let result = num1 + num2
+    return result
+}
+
+const sum1 = add(5, 7)
+console.info(sum1)
+
+const x = (num1, num2) => num1 + num2
+const sum2 = x(5, 7)
+console.info(sum2)
+
